601349966d38f4d944304f29498b7f8980d1a337
vini.d3 <vinicius.sod3@gmail.com>
2023-06-13 10:11:44 -0300

    continuando JS

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,33 @@ const btnDaily = document.getElementById('btn-daily');
 const btnWeekly = document.getElementById('btn-weekly');
 const btnMonthly = document.getElementById('btn-monthly');
 
+const hoursDaily = document.querySelector('.h-daily');
 
 btnDaily.addEventListener('click', function () {
     // console.log(btnDaily, btnWeekly, btnMonthly);
-    function mostrarDaily (){
-        const hoursDaily = document.getElementsByClassName('h-daily');
-        hoursDaily.classList.remove('esconder')
-        hoursDaily.classList.add('mostrar')
-    }
     
+    showHoursDaily ();
+       
+    showPreviousDaily ();
+    
+    const hoursWeekly = document.querySelector('.h-weekly');
+    const hoursMonthly = document.querySelector('.h-monthly');
+    
+    if(hoursDaily.classList.contains('mostrar')){
+        hoursWeekly.classList.remove('mostrar')
+        hoursWeekly.classList.add('esconder')
+        hoursMonthly.classList.remove('mostrar')
+        hoursMonthly.classList.add('esconder')
+    }
+    // FAZER O IF COM O SPAN (PREVIOUS DAILY, WEEKLY E MONTHLY)
 })
+
+function showHoursDaily (){
+    hoursDaily.classList.remove('esconder')
+    hoursDaily.classList.add('mostrar')
+}
+function showPreviousDaily () {
+    const previousDaily = document.querySelector('.p-daily');
+    previousDaily.classList.remove('esconder')
+    previousDaily.classList.add('mostrar')
+}
